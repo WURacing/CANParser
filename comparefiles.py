a092d586160b2feda37bb3ab99e8a4451f8dacf7
Ryan Miller <ryanvmiller@icloud.com>
2018-11-10 13:08:23 -0600

    Working on comparelenghts

--- comparefiles.py
+++ comparefiles.py
@@ -20,7 +20,7 @@ def readCSV():
                 #f.close()
 
     findLength('./ECUCANData/', ECUFiles, LengthsECU)
-    print(LengthsECU)
+    # print(LengthsECU)
 
     for root, dirs, files in os.walk('./OtherCANData/'):
         for file in files:
@@ -32,11 +32,11 @@ def readCSV():
                 #f.close()
 
     findLength('./OtherCANData/', OtherFiles, LengthsOther)
-    print(LengthsOther)
+    # print(LengthsOther)
 
 def findLength(pathPrefix, arr, lengths_arr):
     a = 0
-    print(len(arr))
+    # print(len(arr))
     while a < len(arr):
         with open(pathPrefix + arr[a]) as csv_file:
             #csv_reader = csv.reader(csv_file, delimiter=',')
@@ -48,23 +48,38 @@ def findLength(pathPrefix, arr, lengths_arr):
             except:
                 print("Malformed packet")
                 line_count += 1
-            print(arr[a] + str(line_count))
+            # print(arr[a] + str(line_count))
         lengths_arr.append(line_count)
         a += 1
-    print(lengths_arr)
+    # print(lengths_arr)
 
 #Compares lengths between each file of ECU and Other CAN Data, appends next file in ECU files to current ECU file if the lengths
 #of current file is less than length of corresponding Other file
 def compareLengths(pathPrefix1, pathPrefix2):
-    with open(pathPrefix1) as csv_file1, open(pathPrefix2) as csv_file2:
-        ECUIndex = 0
-        for lOther in LengthsOther:
-            if lOther > LengthsECU[ECUIndex]:
-                ECUWriter = csv.writer(csv_file1)
-                otherWriter = csv.writer(csv_file2)
-                ECUWriter.writerow([LengthsOther[ECUIndex], csv_file2])
-                ECUIndex = ECUIndex + 1
+    loopcount = len(LengthsECU)
+    i = 0
+    j = 0
+    while i < loopcount:
+        if i < len(LengthsECU) and j < len(LengthsOther) and LengthsECU[i] <= LengthsOther[j]:
+            LengthsECU[i] = LengthsECU[i] + LengthsECU[i+1]
+            LengthsECU.remove(LengthsECU[i+1])
+            i -= 1
+        i += 1
+        j += 1
 
-
+print('\n')
 readCSV()
+print('\n')
+print('Before comparing lengths:')
+print('LengthsECU:')
+print(LengthsECU)
+print('LengthsOther:')
+print(LengthsOther)
 compareLengths('./ECUCANData/', './OtherCANData/')
+print('\n')
+print('After comparing lengths:')
+print('LengthsECU:')
+print(LengthsECU)
+print('LengthsOther:')
+print(LengthsOther)
+print('\n')
