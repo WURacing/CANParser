3071c04f3d772f9b7625b01a59f00b1945394356
ethanshry <ehshry@gmail.com>
2019-08-12 21:46:43 -0500

    basic setuptools, license, file restructure

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="can_parser",
+    version="0.1.0",
+    author="WURacing",
+    author_email="wuracing@gmail.com",
+    description="Package to parse CAN Packets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/WURacing/CANParser",
+    packages=setuptools.find_packages(),
+    install_requires=['cantools']
+)
