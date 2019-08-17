f04cf90fa46560b76fad1f4af20e4061da3bd76f
ethanshry <ehshry@gmail.com>
2019-08-17 12:42:56 -0500

    update packages

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/WURacing/CANParser",
-    packages=setuptools.find_packages(),
+    packages=['can_parser'],
     install_requires=['cantools']
 )
